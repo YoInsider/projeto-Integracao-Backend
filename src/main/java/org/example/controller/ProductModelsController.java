cb3d6eb54d7a53c7e0fa0836a9908ad5ca3c4bb6
YoInsider <insensoscoffee17@proton.me>
2025-06-26 13:41:51 -0300

    Melhorias Projeto 04

--- src/main/java/org/example/controller/ProductModelsController.java
+++ src/main/java/org/example/controller/ProductModelsController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/modelos")
+@RequestMapping("/api/models")
 public class ProductModelsController {
 
      @Autowired
